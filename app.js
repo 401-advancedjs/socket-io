fe4411ffacde31ecb42338921b1d86142ad45eff
mestock <MelissaStock44@gmail.com>
2019-06-06 14:25:50 -0700

    added starter code

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
 'use strict';
 
-const fs = require('fs');
+/**
+ * Starts event-driven process
+ * @module app.js
+ *
+ * */
+const socketIOClient = require('socket.io-client');
 
-const alterFile = (file) => {
-  fs.readFile( file, (err, data) => {
-    if(err) { throw err; }
-    let text = data.toString().toUpperCase();
-    fs.writeFile( file, Buffer.from(text), (err, data) => {
-      if(err) { throw err; }
-      console.log(`${file} saved`);
-    });
-  });
-};
+const events = require('./util/events.js');
+const constants = require('./util/constants.js');
+const {readFile, upperCase, writeFile} = require('./modules/read-write.js');
+
+const socket = socketIOClient.connect(constants.SERVER_URL + constants.PORT);
+
+/**
+ * Reads file, converts text to upper case, rewrites file
+ * @function alterFile
+ * @param {string} file path
+ * */
+async function alterFile(file){
+  const read = await readFile(file);
+  const upperCaseValue = upperCase(read);
+  const write = await writeFile(file, upperCaseValue);
+  socket.emit(events.FILE_SAVED_EVENT, write);
+}
 
 let file = process.argv.slice(2).shift();
-alterFile(file);
+alterFile(file).catch(error => {
+  socket.emit(events.FILE_ERROR_EVENT, error);
+});
---
+++ util/events.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * Holds all possible events
+ * @module events
+ * */
+module.exports = {
+    FILE_SAVED_EVENT: 'File-Saved',
+    FILE_ERROR_EVENT: 'File-Error',
+    RECEIVED_SAVE_EVENT: 'received',
+};
+
